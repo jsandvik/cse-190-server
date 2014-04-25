3761a68519e65045fa1575ed5ad55a8a0ccd872a
David Drabik <david@daviddrabik.com>
2014-04-25 06:35:59 +0000

    added Vote class and basic response method

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import render_template, make_response
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:cse190ucsd@localhost/appDB'
@@ -20,9 +21,26 @@ class Student(db.Model):
     def __repr__(self):
         return '<Student %r>' % (self.f_name + self.l_name)
 
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vote = db.Column(db.String(1))
+    question_id = db.Column(db.String(10))
+    pid = db.Column(db.String(10), db.ForeignKey('student.pid'))
+    
+    def __init__(self, vote, pid, question_id):
+        self.vote = vote
+        self.pid = pid
+        self.question_id = question_id
+
+    def __repr__(self):
+        return '<%r voted %r>' % self.pid, self.vote
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+   vote = Vote("a", 123, 0) 
+   db.session.add(vote)
+   db.session.commit()
+   return make_response(render_template('index.html'), 200)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
