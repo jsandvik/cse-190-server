21e29129342ed6542a75a7f8d35e479d8983a317
Jeff Sandvik <jsandvik@gmail.com>
2014-04-26 16:48:57 -0700

    Changes the database url back to what it was originally

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask import render_template, make_response, request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/appDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:cse190ucsd@localhost/appDB'
 db = SQLAlchemy(app)
 
 class Student(db.Model):
