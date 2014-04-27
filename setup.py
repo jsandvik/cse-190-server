62f44d73314d2dee9f48a1aef8b79eb36adcd246
David Drabik <david@daviddrabik.com>
2014-04-26 19:30:35 -0700

    added manifest and setup for package distribution

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='MARKr',
+    version='1.0',
+    long_description=__doc__,
+    packages=['project'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask', 
+        'requests', 
+        'SQLAlchemy', 
+        'Flask-SQLAlchemy', 
+        'MySQL-python', 
+        'fabric'
+    ]
+) 
