b58997a831001f46887ba4808c5f6a868d55f580
David Drabik <david@daviddrabik.com>
2014-04-26 19:15:14 -0700

    server starts from runserver now

--- project/__init__.py
+++ project/__init__.py
@@ -52,5 +52,5 @@ def vote():
 
     return make_response('done', 200)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
