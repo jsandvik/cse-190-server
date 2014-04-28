c2808cf047e12a6b5c1237adb5ca53ddc2f608e8
David Drabik <david@daviddrabik.com>
2014-04-28 09:37:42 -0700

    removed unneeded model

--- markr/__init__.py
+++ markr/__init__.py
@@ -13,21 +13,6 @@ from markr.mod_auth.controllers import mod_auth as auth_module
 # Register blueprints
 app.register_blueprint(auth_module)
 
-# class Student(db.Model):
-#     pid = db.Column(db.String(10), primary_key=True)
-#     l_name = db.Column(db.String(20))
-#     f_name = db.Column(db.String(20))
-#     m_name = db.Column(db.String(20), nullable = True)
-# 
-#     def __init__(self, pid, f_name, m_name, l_name):
-#         self.pid = pid
-#         self.f_name = f_name
-#         self.m_name = m_name
-#         self.l_name = l_name
-# 
-#     def __repr__(self):
-#         return '<Student %r>' % (self.f_name + self.l_name)
-
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vote = db.Column(db.String(1))
