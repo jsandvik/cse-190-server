1d75b1b3a5c3b1a56106e9d52c85de8ce08b9592
David Drabik <david@daviddrabik.com>
2014-04-28 09:42:08 -0700

    added vote module

--- markr/__init__.py
+++ markr/__init__.py
@@ -9,40 +9,11 @@ db = SQLAlchemy(app)
 # Import modules using blueprint handler variable
 # (mod_auth)
 from markr.mod_auth.controllers import mod_auth as auth_module
+from markr.mod_vote.controllers import mod_vote as vote_module
 
 # Register blueprints
 app.register_blueprint(auth_module)
-
-class Vote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    vote = db.Column(db.String(1))
-    question_id = db.Column(db.String(10))
-    pid = db.Column(db.String(10), db.ForeignKey('student.pid'))
-    
-    def __init__(self, vote, pid, question_id):
-        self.vote = vote
-        self.pid = pid
-        self.question_id = question_id
-
-    def __repr__(self):
-        return '<%r voted %r>' % self.pid, self.vote
-
-@app.route('/vote/', methods=['POST'])
-def vote():
-    # Get POST args
-    student_vote = request.form.get('vote', None, str)
-    pid = request.form.get('pid', None, int)
-
-    # deny requests that don't send arguments
-    if not student_vote or not pid:
-        return make_response('error', 400)
-
-    # Insert new vote
-    vote = Vote(student_vote, pid, 0) 
-    db.session.add(vote)
-    db.session.commit()
-
-    return make_response('done', 200)
+app.register_blueprint(vote_module)
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', debug=True)
---
+++ markr/mod_vote/controllers.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, render_template, \
+                  flash, g, session, redirect, url_for
+                  
+from markr import db
+
+from markr.mod_vote.models import Vote
+
+mod_vote = Blueprint('vote', __name__, url_prefix='/vote')
+
+@mod_vote.route('/', methods=['POST'])
+def vote():
+    
+    # Get POST args
+    student_vote = request.form.get('vote', None, str)
+    pid = request.form.get('pid', None, int)
+
+    # deny requests that don't send arguments
+    if not student_vote or not pid:
+        return make_response('error', 400)
+
+    # Insert new vote
+    vote = Vote(student_vote, pid, 0) 
+    db.session.add(vote)
+    db.session.commit()
+
+    return make_response('done', 200)
