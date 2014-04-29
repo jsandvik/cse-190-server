6c84f6f3c695331099e8d38319a826be2e1a4ab1
Jeff Sandvik <jsandvik@gmail.com>
2014-04-29 13:05:08 -0700

    Moves admin templates to templates folder and uses the common base.html

--- markr/teacher_admin/controllers.py
+++ markr/teacher_admin/controllers.py
@@ -4,9 +4,8 @@ from flask import Blueprint, request, render_template, \
 from markr import db
 
 
-teacher_admin = Blueprint('teacher_admin', __name__, 
-                            url_prefix='/admin', template_folder='templates')
+teacher_admin = Blueprint('teacher_admin', __name__, url_prefix='/admin')
 
 @teacher_admin.route('/', methods=['GET'])
 def index():
-    return render_template("questions.html")
+    return render_template("teacher_admin/questions.html")
