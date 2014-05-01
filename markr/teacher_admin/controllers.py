edd062b71d5021cc4bb9434cf9810b8e54d5d916
Jeff Sandvik <jsandvik@gmail.com>
2014-05-01 00:31:54 -0700

    Adds multiple questions on the page, all of the questions in one lecture

--- markr/teacher_admin/controllers.py
+++ markr/teacher_admin/controllers.py
@@ -8,12 +8,17 @@ teacher_admin = Blueprint('teacher_admin', __name__, url_prefix='/admin')
 
 @teacher_admin.route('/', methods=['GET'])
 def index():
-    question = db.session.query(Question).filter(Question.id == 1).one()
-    answers = db.session.query(Answer).filter(Answer.question == 1).all()
+    entries = []
 
+    questions = db.session.query(Question).filter(Question.lecture_id == 1).all()   
     letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    for question in questions:
+        entry = {}
+        answers = db.session.query(Answer).filter(Answer.question == question.id).all()
+        entry["question"] = question
+        entry["answers"] = zip(answers, letters)
+        entries.append(entry)
 
     return render_template("teacher_admin/questions.html", 
-                            question=question, 
-                            answers=zip(answers, letters))
+                            entries=entries)
