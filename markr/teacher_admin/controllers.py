e89e905f74ecea549e7ac2c0a6eb9eb9246d7301
Jeff Sandvik <jsandvik@gmail.com>
2014-04-30 23:04:15 -0700

    displays the values that are currently stored in the database on the question page

--- markr/teacher_admin/controllers.py
+++ markr/teacher_admin/controllers.py
@@ -2,10 +2,13 @@ from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for, make_response
                   
 from markr import db
-
+from markr.models import Question, Answer
 
 teacher_admin = Blueprint('teacher_admin', __name__, url_prefix='/admin')
 
 @teacher_admin.route('/', methods=['GET'])
 def index():
-    return render_template("teacher_admin/questions.html")
+    question = db.session.query(Question).filter(Question.id == 1).one()
+    answers = db.session.query(Answer).filter(Answer.question == 1).all()
+
+    return render_template("teacher_admin/questions.html", question=question, answers=answers)
