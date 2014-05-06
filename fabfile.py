72b95618e7e6c1e67d34ad74a78e74f6ae51bb07
David Drabik <david@daviddrabik.com>
2014-05-05 19:33:28 -0700

    fixed issues with permissions in fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,9 @@ from fabric.api import *
 
 # the user to use for the remote commands
 env.user = 'david'
+
 # the servers where the commands are executed
-env.hosts = ['54.193.1.16']
+env.hosts = ['54.219.169.26']
 env.keyfile = ['$HOME/.ssh/id_rsa.pub']
 
 def pack():
@@ -17,19 +18,23 @@ def deploy():
     put('dist/%s.tar.gz' % dist, '/tmp/markr.tar.gz')
     # create a place where we can unzip the tarball, then enter
     # that directory and unzip it
-    run('rm -rf /tmp/markr')
+    sudo('rm -rf /tmp/markr')
     run('mkdir /tmp/markr')
     with cd('/tmp/markr'):
         run('tar xzf /tmp/markr.tar.gz')
         # now setup the package with our virtual environment's
         # python interpreter
         run('mv ' + dist + '/* .')
+                
         run('/srv/markr/venv/bin/python setup.py install')
+        
+        sudo('chmod 775 /srv/markr/venv/bin/easy_install* /srv/markr/venv/bin/fab* /srv/markr/venv/bin/pip*')
         
         # seed the database
         run('ln -s /srv/markr/config.py config.py')
         run('/srv/markr/venv/bin/python seed_db.py')
          
+        
     # now that all is set up, delete the folder again
     run('rm -rf /tmp/markr /tmp/markr.tar.gz')
     
