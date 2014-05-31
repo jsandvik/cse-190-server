9c5de1ec6829ef6bb6a5a3824e9fbac06de6fc75
Jeff Sandvik <jsandvik@gmail.com>
2014-05-31 15:10:56 -0700

    Adds log in functionality

--- markr/controllers.py
+++ markr/controllers.py
@@ -1,11 +1,19 @@
-from flask import render_template
+from flask import render_template, request, redirect
+from markr.mod_auth.models import Faculty
 from . import app
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """
         Index page for logging in.
     """
+    if request.method == 'POST':
+        teacher_id = request.form.get('id', "", str)
+
+        teacher = Faculty.query.filter_by(ucsd_id=teacher_id).all()
+
+        if teacher:
+            return redirect("admin/classes/{0}".format(teacher_id))
 
 
     return render_template("index.html")
