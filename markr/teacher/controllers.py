aab2deae4a6092d638ded8660fcc51d74fe754ff
Jeff Sandvik <jsandvik@gmail.com>
2014-06-08 19:11:29 -0700

    Adds a function that returns all of the questions in a class at once

--- markr/teacher/controllers.py
+++ markr/teacher/controllers.py
@@ -51,3 +51,27 @@ def get_questions(lecture_id):
         "data" : questions
     }
     return jsonify(data)
+
+@teacher.route('/questions_new/<int:section_id>/', methods=['GET'])
+def get_questions_new(section_id):
+    """
+        This returns a json response of all the questions for a given lecture.
+    """
+    lectures = db.session.query(Lecture).filter(Lecture.sec_id == section_id).all()
+
+    class_questions = []
+    for lecture in lectures:
+        questions = db.session.query(Question).filter(Question.lecture_id == lecture.id).all()
+        questions = [x.serialize for x in questions]
+        class_questions.extend(questions)
+
+    for question in class_questions:
+        answers = Answer.query.filter_by(question=question["id"]).all()
+        question["vote_count"] = Vote.query.filter_by(question_id = question["id"]).count()
+        question["number_of_options"] = len(answers)
+        question["answers"] = [a.serialize for a in answers]
+
+    data = {
+        "data" : class_questions
+    }
+    return jsonify(data)
